372f9433aa40983575bf9d52376b806048de80ae
Mosqueiro <erickcosta2010@gmail.com>
2018-11-24 00:10:34 -0300

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+wget https://github.com/zcore-coin/zcore-source/releases/download/v1.6.3/zcore-1.6.3-x86_64-linux-gnu.tar.gz
+
+service worker01 stop
+
+tar -xzvf zcore-1.6.3-x86_64-linux-gnu.tar.gz
+cd zcore-1.6.3/
+cp * /usr/local/ -a
+rm -rf /home/worker01/.zcore/peers.dat
+
+service worker01 start
+sleep 5
+
+runuser -l worker01 -c 'zcore-cli getinfo'
+runuser -l worker01 -c 'zcore-cli masternode status'
