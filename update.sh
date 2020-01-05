8e0f3acd55c0f1e1805f247cfd91b3d5016b556e
Mosqueiro <erickcosta2010@gmail.com>
2020-01-05 13:58:23 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-wget https://github.com/zcore-coin/zcore-source/releases/download/v1.6.3/zcore-1.6.3-x86_64-linux-gnu.tar.gz
+wget https://github.com/zcore-coin/zcore-2.0/releases/download/v2.0.1/zcore-2.0.1-x86_64-linux-gnu.tar.gz
 
 service worker01 stop
 
-tar -xzvf zcore-1.6.3-x86_64-linux-gnu.tar.gz
-cd zcore-1.6.3/
+tar -xzvf zcore-2.0.1-x86_64-linux-gnu.tar.gz
+cd zcore-2.0.1/
 cp * /usr/local/ -a
 rm -rf /home/worker01/.zcore/peers.dat
 
